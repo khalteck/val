cf7d4f5a3c3b3486596e33041f8edef20342fc63
khalteck <khalidoyeneye@gmail.com>
2024-02-14 02:52:15 +0100

    fixes time

--- src/utils/getTimeOfDay.js
+++ src/utils/getTimeOfDay.js
@@ -1,7 +1,7 @@
 export function getTimeOfDay() {
   const hour = new Date().getHours();
 
-  if (hour >= 5 && hour < 12) {
+  if (hour >= 3 && hour < 12) {
     return "morning";
   } else if (hour >= 12 && hour < 17) {
     return "afternoon";
